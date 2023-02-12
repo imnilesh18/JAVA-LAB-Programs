77930d1e6e3e7de9c1688675ea27a44cf7cac61f
Nilesh Kumar <112562905+imnilesh18@users.noreply.github.com>
2023-02-12 22:54:48 +0530

    Added 12a

---
+++ prg12/AppletP.java
@@ -0,0 +1,14 @@
+//IT WILL WORK ONLY IN JDK VERSION LESS THAN 9 
+package prg12;
+import java.applet.Applet;
+import java.awt.Graphics;
+/*
+<applet code="AppletP.class" width= "300" height="300">
+</applet>
+*/
+public class AppletP extends Applet{
+	public void print(Graphics g)
+	{
+		g.drawString("Welcome", 100, 150);
+	}
+}
